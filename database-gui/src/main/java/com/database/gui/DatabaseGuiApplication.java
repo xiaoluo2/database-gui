25b05ec015b18f3e776f23a6f98202718e460f7f
xluo3 <xluo3@students.towson.edu>
2020-04-30 19:13:00 -0400

    inital commit

---
+++ database-gui/src/main/java/com/database/gui/DatabaseGuiApplication.java
@@ -0,0 +1,13 @@
+package com.database.gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseGuiApplication.class, args);
+	}
+
+}
---
+++ database-gui/src/test/java/com/database/gui/DatabaseGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
